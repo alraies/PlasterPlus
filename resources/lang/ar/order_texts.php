ce0f96cd9bf944075c238888ac7130f0c3a49ff8
alraies <alraies.com@gmail.com>
2022-04-22 19:13:13 +0300

    abd

--- resources/lang/ar/order_texts.php
+++ resources/lang/ar/order_texts.php
@@ -3,7 +3,7 @@
 return [
     'searching_for_delivery_man'=>'Searching for Deliveryman',
     'accepted_by_delivery_man'=>'Accepted by Deliveryman',
-    'preparing_in_restaurants'=>'Preparing in Restaurants',
+    'preparing_in_restaurants'=>تحت التجهيز عند المورد,
     'picked_up'=>'Picked Up',
     'dashboard_order_statistics'=>'Dashboard Order Statistics',
 ];
